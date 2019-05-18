ae78a4fcdf5e7f8dd5fabe38c41fa61c05f3e2d1
Samy Ayoub <samyayoub@gmail.com>
2019-05-18 17:50:34 +0000

    Styling index page; Adding 'New Project' form to create projects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require popper
+//= require tether
+//= bootstrap_sprockets
 //= require_tree .
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,22 @@
 class ProjectsController < ApplicationController
 
     def index
+        @projects = Project.all
+    end
 
+    def new
+        @project = Project.new
+    end
+
+    def create
+        Project.create(project_params)
+        redirect_to root_path
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:name, :description, :due_date)
     end
 
 end
