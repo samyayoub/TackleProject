506f7c9c6ee1a54c817de383f4a2c96dd24867fa
Samy Ayoub <samyayoub@gmail.com>
2019-11-17 03:11:10 +0000

    WIP - working on project and team's controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,25 @@
 class ProjectsController < ApplicationController
-    before_action :authenticate_user!, only: [:create, :show,]
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:create, :show, :edit, :update, :destroy]
 
     def index
-        @projects = Project.all
+        @projects = Project.all.order("created_at DESC")
     end
 
     def new
-        @project = Project.new
+        @project = current_user.projects.build
+        @team = Team.where('id = ?', current_user.team_id)
+    end
+
+    def edit
+        @teams = current_user.teams
     end
 
     def create
         # Project.create(project_params)
         # redirect_to root_path
 
-        @project = current_user.projects.create(project_params)
+        @project = current_user.projects.build(project_params)
         # if @project.valid?
             redirect_to root_path
         # else
@@ -28,8 +34,12 @@ class ProjectsController < ApplicationController
 
     private
 
+    def set_project
+        @project = Project.find(params[:id])
+    end
+
     def project_params
-        params.require(:project).permit(:name, :description, :due_date)
+        params.require(:project).permit(:name, :description, :due_date, :team_id)
     end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ApplicationRecord
     validates :due_date, presence: true
     belongs_to :user
+    belongs_to :team
     has_many :events
 
-    accept_nested_attributes_for :team
+    accepts_nested_attributes_for :team
 end
